b9aa199270843e9f1b9e2168c61e0dee1209c60b
Heloísa Scarante <helotognolli0301@gmail.com>
2025-03-07 20:08:02 -0300

    Update PixPayment.java

--- StrategyPatternPagamento/src/PixPayment.java
+++ StrategyPatternPagamento/src/PixPayment.java
@@ -2,11 +2,11 @@ import java.util.Random;
 
 public class PixPayment implements PaymentStrategy{
     private int geradorCodigoPix(){
-        return new Random().nextInt(50000000); //Gera um código númerico aleatório como código PIX (não consegui incluir letras)
+        return new Random().nextInt(50000000); //Informa o código pix randomicamente 
     }
 
     @Override
-    public void processPayment(double amount){ //uso do metodo da Interface PaymentSrategy (bem simples, apenas para demonstrar o funcionamento)
+    public void processPayment(double amount){ //usa o metodo da interface para visualizar seu funcionamento
         System.out.println("Método PIX escolhido.");
         System.out.println("Código PIX: " + geradorCodigoPix());
         System.out.println("Pagamento de " + amount + " realizado com sucesso!");
