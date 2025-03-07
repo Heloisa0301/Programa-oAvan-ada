7ae81087003cb7e938487c76b5a8a1f55e472e68
Heloísa Scarante <helotognolli0301@gmail.com>
2025-03-07 20:31:20 -0300

    Update NotificationFactory.java

--- FactoryMethodNotificacao/src/NotificationFactory.java
+++ FactoryMethodNotificacao/src/NotificationFactory.java
@@ -2,7 +2,7 @@ public class NotificationFactory{
     public static Notification createNotification(String type){
         switch(type){
             case "email":
-                return new EmailNotification();
+                return new EmailNotification();//a classe fabrica instanciando para que nao seja diretamente feito na classe main padronizando o processo
 
             case "sms":
                 return new SMSNotification();
